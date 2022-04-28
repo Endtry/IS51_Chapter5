42382adb30efcb4e2849aea759fb5387278b9d4f
endtry <lucas123phan@gmail.com>
2022-04-28 15:07:29 -0700

    adding open file codes

--- 5_1.py
+++ 5_1.py
@@ -3,30 +3,52 @@ file_name = "./infile.txt"
 infile = open(file_name, "r")
 names = [line.rstrip() for line in infile]
 
-infile.close()
+# Open a specified file in read mode
+# infile.close()
+
+# def main():
+#     display_with_forloop(file_name)
+#     print()
+#     display_with_list_comprehension(file_name)
 
 
-def main():
-    display_with_forloop(file_name)
-    print()
-    display_with_list_comprehension(file_name)
+# def display_with_forloop(file_name):
+#     infile = open_file(file_name, "r")
+#     for line in infile:
+#         print(line, end="")
+#     infile.close()
 
 
-def display_with_forloop(file_name):
-    infile = open_file(file_name, "r")
-    for line in infile:
-        print(line, end="")
-    infile.close()
+# def display_with_list_comprehension(file_name):
+#     infile = open_file(file_name, "r")
+#     items = [line.rstrip() for line in infile]
+#     print("items: ", items)
+#     infile.close()
 
 
-def display_with_list_comprehension(file_name):
-    infile = open_file(file_name, "r")
-    items = [line.rstrip() for line in infile]
-    print("items: ", items)
-    infile.close()
+# def open_file(file_name, mode):
+#     return open(file_name, mode)
+
+# main()
+
+# Create a file object in write mode
 
 
-def open_file(file_name, mode):
-    return open(file_name, mode)
+# def main():
+#     save_to_outfile("./outfile.txt")
 
-main()
+
+# def open_file(file_name, mode):
+#     return open(file_name, mode)
+
+
+# def save_to_outfile(file_name):
+#     outfile = open_file(file_name, "w")
+#     for name in names:
+#         if "Doe" not in name:
+#             outfile.write(name + "\n")
+#     outfile.close()
+
+
+# main()
+
